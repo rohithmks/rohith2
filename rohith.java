8536e556ce28fd33ca4fcf4e8920441d1570ad51
rohithmks <rohithmks20@gmail.com>
2026-02-28 13:03:53 +0530

    Update rohith.java

--- rohith.java
+++ rohith.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // This line prints the message to the console
-        System.out.println("Hello, World!");
+        System.out.println("Hello");
     }
 }
